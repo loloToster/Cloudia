e1897fba3fb25fdf447b9ce3e8d80b9d58c50bbe
loloToster <lolo.toster4@gmail.com>
2022-09-02 15:28:00 +0200

    💄 added add button in image list

--- client/src/components/ImageList.tsx
+++ client/src/components/ImageList.tsx
@@ -30,7 +30,8 @@ function ImageList() {
         navigate("/image/" + id)
     }
 
-    const handleDelete = async (id: string) => {
+    const handleDelete = async (e: React.MouseEvent, id: string) => {
+        e.stopPropagation()
         let res = await fetch("/api/image/" + id, { method: "DELETE" })
         if (res.ok) setImages(images.filter(img => img.id !== id))
     }
@@ -50,12 +51,12 @@ function ImageList() {
                             <div className="images__title">{img.title}</div>
                             <div className="images__user">{img.user}</div>
                         </div>
-                        <a href={`/cdn/${img.file}`} download={img.file} className="images__button images__button--download">
+                        <a onClick={e => e.stopPropagation()} href={`/cdn/${img.file}`} download={img.file} className="images__button images__button--download">
                             <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24">
                                 <path d="M6 20q-.825 0-1.412-.587Q4 18.825 4 18v-3h2v3h12v-3h2v3q0 .825-.587 1.413Q18.825 20 18 20Zm6-4-5-5 1.4-1.45 2.6 2.6V4h2v8.15l2.6-2.6L17 11Z" />
                             </svg>
                         </a>
-                        <button onClick={() => handleDelete(img.id)} className="images__button images__button--delete">
+                        <button onClick={e => handleDelete(e, img.id)} className="images__button images__button--delete">
                             <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24">
                                 <path d="M7 21q-.825 0-1.412-.587Q5 19.825 5 19V6H4V4h5V3h6v1h5v2h-1v13q0 .825-.587 1.413Q17.825 21 17 21ZM17 6H7v13h10ZM9 17h2V8H9Zm4 0h2V8h-2ZM7 6v13Z" />
                             </svg>
@@ -63,6 +64,11 @@ function ImageList() {
                     </div>
                 </div>
             ))}
+            {!loading && (
+                <div onClick={() => navigate("/add")} className="images__add">
+                    <div className="images__add__plus"></div>
+                </div>
+            )}
         </div>
     )
 }
