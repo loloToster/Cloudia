7872f7a72b8c009ae8c16601e42b3dcfe7b6011c
loloToster <lolo.toster4@gmail.com>
2022-09-08 18:05:32 +0200

    🚧 create ui base for text item

--- client/src/components/FileList.tsx
+++ client/src/components/FileList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { FileJson } from "@backend-types/types"
 
@@ -56,6 +56,23 @@ function FileList() {
         })
     }
 
+    const firstActionBtn = useRef<HTMLButtonElement>(null)
+    const secondActionBtn = useRef<HTMLButtonElement>(null)
+    const textarea = useRef<HTMLTextAreaElement>(null)
+
+    const handleQuickTextOpen = () => {
+        firstActionBtn.current?.classList.add("hidden")
+        secondActionBtn.current?.classList.add("active")
+        textarea.current?.focus()
+    }
+
+    const handleQuickTextClose = (e: React.MouseEvent) => {
+        firstActionBtn.current?.classList.remove("hidden")
+        secondActionBtn.current?.classList.remove("active")
+        if (textarea.current) textarea.current.value = ""
+        e.stopPropagation()
+    }
+
     return (
         <div className="files">
             {loading && [...Array(5)].map((_, i) => (
@@ -63,16 +80,33 @@ function FileList() {
             ))}
             {!loading && (
                 <div className="files__quick-actions">
-                    <div onClick={handleQuickUpload} className="files__quick-action">
+                    <button ref={firstActionBtn} onClick={handleQuickUpload} className="files__quick-action">
                         <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48">
                             <path d="M11 40q-1.2 0-2.1-.9Q8 38.2 8 37v-7.15h3V37h26v-7.15h3V37q0 1.2-.9 2.1-.9.9-2.1.9Zm11.5-7.65V13.8l-6 6-2.15-2.15L24 8l9.65 9.65-2.15 2.15-6-6v18.55Z" />
                         </svg>
-                    </div>
-                    <div className="files__quick-action">
+                    </button>
+                    <button ref={secondActionBtn} onClick={handleQuickTextOpen} className="files__quick-action">
                         <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48">
                             <path d="M24 42v-3.55l10.8-10.8 3.55 3.55L27.55 42ZM6 31.5v-3h15v3Zm34.5-2.45-3.55-3.55 1.45-1.45q.4-.4 1.05-.4t1.05.4l1.45 1.45q.4.4.4 1.05t-.4 1.05ZM6 23.25v-3h23.5v3ZM6 15v-3h23.5v3Z" />
                         </svg>
-                    </div>
+                        <div className="files__quick-action__textarea-wrapper">
+                            <textarea ref={textarea} placeholder="Type in your text here..."></textarea>
+                            <div>
+                                <div onClick={handleQuickTextClose} className="action-btn">Cancel</div>
+                                <div className="action-btn">
+                                    <div className="action-btn__content">
+                                        Save
+                                    </div>
+                                    <div className="action-btn__loading">
+                                        Saving
+                                        <span className="action-btn__loading-item">.</span>
+                                        <span className="action-btn__loading-item">.</span>
+                                        <span className="action-btn__loading-item">.</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </button>
                 </div>
             )}
             {!loading && files.map(file => {
