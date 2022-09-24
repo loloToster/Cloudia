e59ddb058844df242a20b2e2d07fd00b580108ce
loloToster <lolo.toster4@gmail.com>
2022-09-24 21:48:11 +0200

    🚸 added copy success indicator in text item

--- client/src/components/ItemList/TextItem.tsx
+++ client/src/components/ItemList/TextItem.tsx
@@ -1,3 +1,4 @@
+import { useRef, MouseEvent } from "react"
 import { TextJson } from "@backend-types/types"
 
 // https://stackoverflow.com/questions/71873824/copy-text-to-clipboard-cannot-read-properties-of-undefined-reading-writetext
@@ -49,8 +50,23 @@ function TextItem(props: { textItem: TextJson, removeItem: Function }) {
         if (res.ok) removeItem(id)
     }
 
-    const handleCopy = () => {
-        copyToClipboard(textItem.text)
+    const copyBtn = useRef<HTMLButtonElement>(null)
+
+    let copyTimeout: any
+    const handleCopy = (e: MouseEvent) => {
+        const button = copyBtn.current
+        if (!button) return
+
+        clearTimeout(copyTimeout)
+        copyToClipboard(textItem.text);
+        button.classList.remove("success")
+        // restarts color animation
+        void button.querySelector("svg:nth-child(2)")?.scrollHeight
+        button.classList.add("success")
+
+        copyTimeout = setTimeout(() => {
+            button.classList.remove("success")
+        }, 3000)
     }
 
     return (<div className="text-item">
@@ -64,9 +80,12 @@ function TextItem(props: { textItem: TextJson, removeItem: Function }) {
                     <path d="M7 21q-.825 0-1.412-.587Q5 19.825 5 19V6H4V4h5V3h6v1h5v2h-1v13q0 .825-.587 1.413Q17.825 21 17 21ZM17 6H7v13h10ZM9 17h2V8H9Zm4 0h2V8h-2ZM7 6v13Z"></path>
                 </svg>
             </button>
-            <button onClick={handleCopy}>
+            <button onClick={handleCopy} ref={copyBtn}>
                 <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24">
                     <path d="M5 22q-.825 0-1.413-.587Q3 20.825 3 20V6h2v14h11v2Zm4-4q-.825 0-1.412-.587Q7 16.825 7 16V4q0-.825.588-1.413Q8.175 2 9 2h9q.825 0 1.413.587Q20 3.175 20 4v12q0 .825-.587 1.413Q18.825 18 18 18Zm0-2h9V4H9v12Zm0 0V4v12Z" />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24">
+                    <path d="m9.55 18.55-6.3-6.3 1.875-1.875L9.55 14.8l9.375-9.375L20.8 7.3Z" />
                 </svg>
             </button>
         </div>
