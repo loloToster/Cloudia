6ddcb18732d239f905f85841824248ff39a01505
loloToster <lolo.toster4@gmail.com>
2022-09-24 16:08:48 +0200

    💄 added progress indicator to action btn component

--- client/src/components/ActionBtn.tsx
+++ client/src/components/ActionBtn.tsx
@@ -1,10 +1,11 @@
-import { MouseEvent } from "react"
+import { MouseEvent, CSSProperties } from "react"
 
 interface Props {
     text: string,
     onClick: ((e: MouseEvent) => any) | string,
     textLoading: string,
     loading: boolean,
+    progress: number,
     download: string,
     newPage: boolean,
     className: string
@@ -16,6 +17,7 @@ function ActionBtn(props: Partial<Props>) {
         onClick: clickHandler,
         textLoading = "Loading",
         loading = false,
+        progress,
         download,
         newPage = false,
         className
@@ -32,12 +34,21 @@ function ActionBtn(props: Partial<Props>) {
         onClick = clickHandler
     }
 
+    const progressPresent = typeof progress === "number"
+
+    if (progressPresent && (progress < 0 || progress > 1))
+        throw Error("Progress must be a float between 0 & 1")
+
+    const progressPercentage = Math.round((progress || 0) * 100)
+
     return (<Tag onClick={onClick}
         href={href}
         download={download}
         className={`action-btn ${loading ? "loading" : ""} ${className || ""}`}
         target={newPage ? "_blank" : undefined}
         rel={newPage ? "noreferrer" : undefined}>
+        <div className="action-btn__progress"
+            style={{ "--value": progress } as CSSProperties}></div>
         <span className="action-btn__content">
             {text}
         </span>
@@ -46,6 +57,7 @@ function ActionBtn(props: Partial<Props>) {
             <span className="action-btn__loading-item">.</span>
             <span className="action-btn__loading-item">.</span>
             <span className="action-btn__loading-item">.</span>
+            {progressPresent ? ` (${progressPercentage}%)` : ""}
         </span>
     </Tag>)
 }
