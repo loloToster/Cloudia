76260814d6cf8ff38fe9e1089ab7f59c7cff56fa
loloToster <lolo.toster4@gmail.com>
2022-09-25 13:25:39 +0200

    🚧 added UploadItem component

---
+++ client/src/components/ItemList/UploadItem.tsx
@@ -0,0 +1,29 @@
+import { CSSProperties } from "react"
+
+function UploadItem(props: { numberOfFiles: number, progress: number }) {
+    const { numberOfFiles, progress } = props
+
+    if (progress < 0 || progress > 1)
+        throw Error("Progress must be a float between 0 & 1")
+
+    const progressPercentage = Math.round(progress * 100)
+
+    let fileNaming = "file"
+    if (numberOfFiles > 1) fileNaming += "s"
+
+    return (<div className="upload-item">
+        <div className="upload-item__title">
+            Uploading {numberOfFiles} {fileNaming}
+            <span className="upload-item__loading-item">.</span>
+            <span className="upload-item__loading-item">.</span>
+            <span className="upload-item__loading-item">.</span>
+        </div>
+        <div className="upload-item__progress">
+            <div className="upload-item__progress__circle" style={{ "--progress": progress } as CSSProperties}>
+                <div className="upload-item__progress__inner-circle">{progressPercentage}%</div>
+            </div>
+        </div>
+    </div>)
+}
+
+export default UploadItem
