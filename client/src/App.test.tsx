bfdcd0f7006662015ba8df63074ff53f909573f2
loloToster <lolo.toster4@gmail.com>
2022-09-30 23:54:54 +0200

    🧪 setup base for testing

---
+++ client/src/App.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react"
+import App from "./App"
+
+test("renders logo", () => {
+  render(<App />)
+  const logo = screen.getByText("Cloudia")
+  expect(logo).toBeInTheDocument()
+})
