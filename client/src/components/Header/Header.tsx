f1e592e4f623d2d3ccdac87c26772e649bf248a2
loloToster <lolo.toster4@gmail.com>
2024-08-08 14:24:42 +0200

    ✨ created basic client-side search

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,28 +1,17 @@
-import { useEffect, useRef, useState } from "react"
-import { Link, useLocation, useNavigate, useSearchParams } from "react-router-dom"
+import { useEffect, useRef } from "react"
+import { Link, useLocation } from "react-router-dom"
+
+import routes from "src/routes"
+import { useSearch } from "src/contexts/searchContext"
 
 import "./Header.scss"
 
-import routes from "src/routes"
-
-const QUERY_KEY = "q"
-const searchableRoutes = routes.filter(r => r.searchable && r.path).map(r => r.path) as string[]
-
 function Header() {
     const location = useLocation()
-    const navigate = useNavigate()
 
-    const [searchParams, setSearchParams] = useSearchParams();
-    const [searchQuery, setSearchQuery] = useState(searchParams.get(QUERY_KEY) || "")
+    const { searchQuery, setSearchQuery } = useSearch()
 
     const searchInput = useRef<HTMLInputElement>(null)
-
-    useEffect(() => {
-        const param = searchParams.get(QUERY_KEY)
-
-        if (param !== searchQuery)
-            setSearchQuery(param || "")
-    }, [searchParams])
 
     const handleSearchEnter = () => {
         searchInput.current?.focus()
@@ -33,41 +22,40 @@ function Header() {
         searchInput.current?.focus()
     }
 
-    const handleSearch = (e: React.KeyboardEvent) => {
-        if (e.key !== "Enter") return
-
-        if (!searchableRoutes.includes(location.pathname))
-            navigate(searchableRoutes[0])
-
-        setSearchParams({ [QUERY_KEY]: searchQuery })
-    }
+    // clear search on page change
+    useEffect(() => {
+        setSearchQuery("")
+    }, [location.pathname, setSearchQuery])
 
     return (
         <div className="header">
             <Link to="/" className="header__logo">
                 Cloudia
             </Link>
-            <div className="header__search">
-                {searchQuery ? (
-                    <button onClick={handleSearchClear}>
-                        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
-                            <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
-                        </svg>
-                    </button>
-                ) : (
-                    <button onClick={handleSearchEnter}>
-                        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
-                            <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
-                        </svg>
-                    </button>
-                )}
-                <input type="text"
-                    ref={searchInput}
-                    value={searchQuery}
-                    onChange={e => setSearchQuery(e.target.value)}
-                    onKeyPress={handleSearch}
-                    placeholder="Search Cloudia" />
-            </div>
+            {routes.find(r => r.path === location.pathname)?.searchable ? (
+                <div className="header__search">
+                    {searchQuery ? (
+                        <button onClick={handleSearchClear}>
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
+                                <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+                            </svg>
+                        </button>
+                    ) : (
+                        <button onClick={handleSearchEnter}>
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
+                                <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
+                            </svg>
+                        </button>
+                    )}
+                    <input type="text"
+                        ref={searchInput}
+                        value={searchQuery}
+                        onChange={e => setSearchQuery(e.target.value)}
+                        placeholder="Search Cloudia" />
+                </div>
+            ) : (
+                <div className="header__spacer"></div>
+            )}
             <Link to="/trash" className="header__secondary">
                 <span>Trash</span>
                 <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24">
