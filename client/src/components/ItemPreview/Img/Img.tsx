91513942e85dda843d4ed145002b75f3c84ac748
loloToster <lolo.toster4@gmail.com>
2024-09-03 14:05:52 +0200

    🚸 improved img preview

--- client/src/components/ItemPreview/Img/Img.tsx
+++ client/src/components/ItemPreview/Img/Img.tsx
@@ -1,12 +1,44 @@
+import { useEffect, useRef } from "react"
+import { TransformWrapper, TransformComponent, useControls } from "react-zoom-pan-pinch"
+
 import { PreviewProps } from "../ItemPreview"
+
 import "./Img.scss"
 
-function Img(props: PreviewProps) {
+function ImgInner(props: PreviewProps) {
   const { item } = props
+  const { centerView } = useControls()
+
+  const lastItemId = useRef("")
+
+  useEffect(() => {
+    if (lastItemId.current === item.id) return
+    lastItemId.current = item.id
+    centerView(1, 0)
+  }, [item, centerView])
 
   return (
-    <div className="pre-img">
+    <TransformComponent>
       <img alt={item.title} src={`/cdn/${item.id}`} />
+    </TransformComponent>
+  )
+}
+
+function Img(props: PreviewProps) {
+  return (
+    <div className="pre-img">
+      <TransformWrapper
+        limitToBounds={false}
+        wheel={{ step: 1, smoothStep: 0.005 }}
+        onZoomStop={
+          (ref) => {
+            if (ref.state.scale === 1) {
+              ref.centerView()
+            }
+          }
+        }>
+        <ImgInner {...props} />
+      </TransformWrapper>
     </div>
   )
 }
