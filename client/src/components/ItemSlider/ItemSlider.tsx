2371009ddbcd43ebfce6619744cd756592e289bf
loloToster <lolo.toster4@gmail.com>
2024-09-05 10:44:17 +0200

    💄 added basic item actions in item slider

--- client/src/components/ItemSlider/ItemSlider.tsx
+++ client/src/components/ItemSlider/ItemSlider.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import { ClientItem } from "@backend-types/types";
 import { useItemList } from "src/contexts/itemListContext";
@@ -6,8 +6,19 @@ import ItemPreview from "../ItemPreview/ItemPreview";
 
 import "./ItemSlider.scss";
 
+type IdFunc = (id: string) => void;
+
 function ItemSlider() {
-  const { displayItems, itemSliderInit, closeSlider } = useItemList();
+  const {
+    displayItems,
+    itemSliderInit,
+    closeSlider,
+    handlePin,
+    handleUnpin,
+    handleDelete,
+    handleTrash,
+    handleRestore,
+  } = useItemList();
 
   const [idx, setIdx] = useState(
     itemSliderInit
@@ -16,9 +27,35 @@ function ItemSlider() {
   );
   const [item, setItem] = useState<ClientItem | null>(null);
 
+  const movingItem = useRef<string | null>(null);
+
   useEffect(() => {
-    setItem(displayItems[idx] ?? null);
+    if (movingItem.current) {
+      let newIdx = displayItems.findIndex((i) => i.id === movingItem.current);
+      newIdx = newIdx < 0 ? idx : newIdx;
+      setIdx(newIdx);
+      setItem(displayItems[newIdx] ?? null);
+    } else {
+      setItem(displayItems[idx] ?? null);
+    }
   }, [idx, displayItems]);
+
+  const onMoveAction = (action: IdFunc) => {
+    if (!item) return;
+    movingItem.current = item.id;
+    action(item.id);
+  };
+
+  const onRemovalAction = (action: IdFunc) => {
+    if (!item) return;
+    action(item.id);
+    closeSlider();
+  };
+
+  const onChangeIdx = (offset: number) => {
+    movingItem.current = null;
+    setIdx((prev) => prev + offset);
+  };
 
   return (
     <div className="item-slider">
@@ -39,14 +76,57 @@ function ItemSlider() {
                 alt=""
               />
             )}
-            <div>{item.title}</div>
+            <div className="sitem__header__title">{item.title}</div>
+            {item.pinned ? (
+              <button title="Unpin" onClick={() => onMoveAction(handleUnpin)}>
+                <span className="material-symbols-rounded pinned">keep</span>
+              </button>
+            ) : (
+              <button title="Pin" onClick={() => onMoveAction(handlePin)}>
+                <span className="material-symbols-rounded">keep</span>
+              </button>
+            )}
+            {(item.type === "img" || item.type === "file") && (
+              <button title="Download">
+                <a href={`/cdn/${item.id}`} download={item.title}>
+                  <span className="material-symbols-rounded">download</span>
+                </a>
+              </button>
+            )}
+            {item.trashed ? (
+              <>
+                <button
+                  title="Restore"
+                  onClick={() => onRemovalAction(handleRestore)}
+                >
+                  <span className="material-symbols-rounded">
+                    restore_from_trash
+                  </span>
+                </button>
+                <button
+                  title="Delete Permanently"
+                  onClick={() => onRemovalAction(handleDelete)}
+                >
+                  <span className="material-symbols-rounded">
+                    delete_forever
+                  </span>
+                </button>
+              </>
+            ) : (
+              <button
+                title="Move to trash"
+                onClick={() => onRemovalAction(handleTrash)}
+              >
+                <span className="material-symbols-rounded">delete</span>
+              </button>
+            )}
           </div>
           <div className="sitem__preview">
             <ItemPreview item={item} />
           </div>
           {idx !== 0 && (
             <button
-              onClick={() => setIdx((prev) => prev - 1)}
+              onClick={() => onChangeIdx(-1)}
               className="sitem__arrow left"
             >
               <span className="material-symbols-rounded">
@@ -56,7 +136,7 @@ function ItemSlider() {
           )}
           {idx !== displayItems.length - 1 && (
             <button
-              onClick={() => setIdx((prev) => prev + 1)}
+              onClick={() => onChangeIdx(1)}
               className="sitem__arrow right"
             >
               <span className="material-symbols-rounded">
